a4350f119e447faa0334ea53b3a363209c0d1b35
Jordan Ramirez <jordan.s.ramirez@gmail.com>
2026-07-01 21:55:08 -0400

    first commit

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: [
+    'module:@react-native/babel-preset',
+    'nativewind/babel',
+  ],
+  // Must be the last plugin. Powers Reanimated 4 and react-native-live-markdown.
+  plugins: ['react-native-worklets/plugin'],
 };
